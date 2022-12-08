a3dcd73294ee5c2f9c0a7f4629c650f65fb4b250
kdshola <khidirshola@gmail.com>
2022-12-08 20:26:58 +0100

    print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19", 2015-10-19",
  * followed by a new line, to standard error.
- * Return: Always 0 (success)
+ * Return: 0 Always (success)
  */
 int main(void)
 {
