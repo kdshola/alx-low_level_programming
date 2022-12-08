b99f2217b46d62b0a37cd161a18e3020b72e0e73
kdshola <khidirshola@gmail.com>
2022-12-08 07:04:21 -0800

    Print the sizes of the various input types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sizes of the various input types
+ * retutn: 0 if successful, else non-zero
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}
