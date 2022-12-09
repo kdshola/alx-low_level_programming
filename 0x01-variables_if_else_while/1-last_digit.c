115f871ac50b32e2722eaf59d166e155a2fb38ac
kdshola <khidirshola@gmail.com>
2022-12-09 12:09:52 -0800

    Print last digit and state if it is 0, less than or greater than six

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - assign random number to variable n and print
+ *the last digit of the number stored in n
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	x = n % 10;
+	printf("Last digit of %d is %d ", n, x);
+	if (x > 5)
+	{
+		printf("and is greatee than 5");
+	}
+	if (x == 0)
+	{
+		printf("and is 0");
+	}
+	if (x < 6 && x != 0)
+	{
+		printf("and is less than six and not 0");
+	}
+	return (0);
+}
