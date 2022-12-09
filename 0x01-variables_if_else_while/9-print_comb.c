b48a99e393f2cb3af69c51ba5db3903477b4bacb
kdshola <khidirshola@gmail.com>
2022-12-09 15:13:28 -0800

    print num combinatiom

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all combinations of given digit
+ * followed ny comma amd newline
+ * Return: Alwats 0
+ */
+int main(void)
+{
+	int com;
+
+	for (com = 0; com <= 9; com++)
+	{
+		putchar((com % 10) + '0)';
+
+		if (com == 9)
+			continiue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
