21d66563d6bfc173e9b6578f375d363aa3a067a6
kdshola <khidirshola@gmail.com>
2022-12-14 06:29:11 -0800

    print times table from given number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ * @n: numberto start from
+ */
+void print_times_table(int n)
+{
+	int num, mul, ans;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mul = 1; mul <= n; mul++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				ans = num * mul;
+
+				if (ans <= 99)
+					_putchar(' ');
+				if (ans <= 9)
+					_putchar(' ');
+				if (ans >= 100)
+				{
+					_putchar((ans / 100) + '0');
+					_putchar(((ans / 10)) % 10 + '0');
+				}
+				else if (ans <= 99 && ans >= 10)
+				{
+					_putchar((ans / 10) + '0');
+				}
+				_putchar((ans % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
