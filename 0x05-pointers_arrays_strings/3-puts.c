f282f5e58a395809ae477b05b3dd74108f93b3f7
kdshola <khidirshola@gmail.com>
2022-12-19 11:35:17 -0800

    print string length

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string followed by new line
+ * @str: string parameter
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int a = 0;
+	while (*str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
