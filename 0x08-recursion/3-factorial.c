fcb256dfee65da3499cf9e7aa452512649419616
kdshola <khidirshola@gmail.com>
2023-01-04 17:56:36 +0100

    Recursive functions

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - prints factorial of an int
+ * @n: int
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	int x;
+
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		x = n * factorial(n - 1);
+	return (x);
+}
