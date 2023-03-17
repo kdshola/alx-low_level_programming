2d3efdfb5603078d74e72ae3146646504ac59250
kdshola <khidirshola@gmail.com>
2023-03-17 21:36:35 +0100

    Print all possible combinations of two two-digit numbers from 0 to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print all possible combinations of 2 2-digits from 0 - 99
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; m < 99; m++)
+	{
+		for (n = m + 1; n < 100; n++)
+		{
+			putchar(m / 10 + '0');
+			putchar(m % 10 + '0');
+			putchar(32);
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
+			if (m != 98 && n != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
