55ec43c9b56948f5ce0327fd6cc8b0b880877b5d
kdshola <khidirshola@gmail.com>
2023-03-22 20:43:42 +0100

    Find and print the sum of the even-valued terms in fibonacci sequence, followed by a new line

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonnaci numbers less than 4,000,000
+ * followed by a new line
+ * Return: 0 always
+ */
+int main(void)
+{
+	unsigned long fib = 0, fib1 = 1, fisum;
+	unsigned long sum = 0;
+
+	while (1)
+	{
+		fisum = fib + fib1;
+		if (fisum > 4000000)
+			break;
+		if ((fisum % 2) == 0)
+			sum += fisum;
+		fib = fib1;
+		fib1 = fisum;
+	}
+	printf("%lu\n", sum);
+}
