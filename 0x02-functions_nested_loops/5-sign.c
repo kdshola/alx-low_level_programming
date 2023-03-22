cbdad2eaf86f399ce4fcb3c306fa97d637617523
kdshola <khidirshola@gmail.com>
2023-03-22 17:33:46 +0100

    Print the sign of a number and returns 1 if n is greater than zero, returns 0 if n is zero, returns -1 if n is less than zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ * @n: number to check its sign
+ * Return: 1 for positive, 0 for zero, -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putcahr('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putcahr('-');
+		return (-1);
+	}
+}
