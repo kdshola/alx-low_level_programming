a13f564a6a89111c16d272fabee232d7205b2327
kdshola <khidirshola@gmail.com>
2023-04-03 11:41:11 +0100

    Create 100-set_string.c and 101-crackme_password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets a doube pointer to char
+ * @s: pointer to set to char
+ * @to: pointer to set to
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
