3ddf4b9e4b672d150486277fc25a120bb0913ff7
kdshola <khidirshola@gmail.com>
2023-04-11 17:41:19 +0100

    Create 100-argsostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates arguments to amin
+ * @ac: argument count
+ * @av: argument vector
+ * Return: pointer to concatenated string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, len, k = 0;
+	char *new;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+	len++
+	new = (char *)malloc(sizeof(char) * len);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			new[k] = av[i][j];
+			k++;
+		}
+		new[k] = '\n';
+		k++;
+	}
+	new[k] = '\0';
+	return (new);
+}
