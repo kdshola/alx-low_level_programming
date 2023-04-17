e9ef4ab6d05192663f8ef0c9b9321c842f573c02
kdshola <khidirshola@gmail.com>
2023-04-17 20:37:10 +0100

    Create Readme.md, 0-object_like_macro.h, 1-pi.h, 2-main.c, 3-function_like_macro.h and 4-sum.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints file name followed by a new line
+ * Return: 0 Always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
