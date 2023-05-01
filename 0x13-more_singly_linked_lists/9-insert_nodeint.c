c2822946b51046af6df660ddf8f6d682eb2792f0
kdshola <khidirshola@gmail.com>
2023-05-01 14:47:34 +0100

    Create 9-insert_nodeint.c and 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int index = 1;
 	listint_t *new, *ptr = *head;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
