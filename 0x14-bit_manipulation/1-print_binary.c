748053c7bbaff40003b0a0685ae51ec7b09ee422
kdshola <khidirshola@gmail.com>
2023-05-04 18:47:51 +0100

    Create 0-binary_to_uint.c, 1-print_binary.c and 100-get_endianness.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,10 +10,10 @@ void print_binary(unsigned long int n)
 	unsigned int shift;
 	int i;
 
-	for (i = 63; i >= 0; i--)
+	for (i = 31; i >= 0; i--)
 	{
-		shift = 1 << i;
-		if (shift & n)
+		shift = n >> i;
+		if (shift & 1)
 			_putchar('1');
 		else
 			_putchar('0');
